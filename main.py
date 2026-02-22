28ba541c151801d5c25244ab0c02cea93dab0255
Vitalizatr <96489920+Vitalizatr@users.noreply.github.com>
2026-02-22 22:19:35 +0200

    a

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-dotenv_path = os.path.join(os.path.dirname(__file__), 'token.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
-
-
 import asyncio
 import logging
 import sys
